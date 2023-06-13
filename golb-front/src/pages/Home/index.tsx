d41dce97972f32c6ed5527322bf995dbb5435fc3
yuanshuai <yuanshuai@gyyx.cn>
2023-06-13 09:47:11 +0800

    调整样式

--- golb-front/src/pages/Home/index.tsx
+++ golb-front/src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import BlogPageList from "../../components/BlogPageList";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
     return (
         <div>
-
+            <div style={{textAlign: "center", marginTop: 40, marginBottom: 30}}>
+                <Typography variant="h4" gutterBottom>
+                    aabb的文档
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                    这是aabb的文档，balabalabala...
+                </Typography>
+                <div>图标图标图标</div>
+            </div>
             <BlogPageList/>
         </div>
     );
